62d7b3ba7990cd57a8cd26d53574b5a712117444
nsanta03 <nick.santanjelo@gmail.com>
2020-05-17 16:57:54 +0000

    commit

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require "httparty"
 def view(template); erb template.to_sym; end
 
 get "/" do
+  ### Get the weather
   lat = 42.0574063
   long = -87.6722787
 
@@ -13,6 +14,14 @@ get "/" do
 
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{weather_key}"
   @forecast = HTTParty.get(url).parsed_response.to_hash
+
+  puts "It is currently #{forecast["current"]["temp"]} degrees and #{forecast["current"]["weather"][0]["description"]} at the Kellogg Global Hub"
+  puts "Three Day Forceast:"
+  for day in forecast["daily"]
+    puts "Tomorrow will be #{1["temp"]["day"]} degrees and #{1["weather"]["description"]}"
+    puts "The day after will be #{2["temp"]["day"]} degrees and #{2["weather"]["description"]}"
+    puts "The following day will be #{3["temp"]["day"]} degrees and #{3["weather"]["description"]}"
+  end
 
   ### Get the news
   news_key = "506191c9c05f4e31baf54ff587752cee"
