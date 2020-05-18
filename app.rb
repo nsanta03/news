6abafd6847a4ff180ed68c5d71e50e79699bc4d9
nsanta03 <nick.santanjelo@gmail.com>
2020-05-18 02:03:43 +0000

    commit

--- app.rb
+++ app.rb
@@ -15,6 +15,7 @@ get "/" do
   url = "https://api.openweathermap.org/data/2.5/onecall?lat=#{lat}&lon=#{long}&units=#{units}&appid=#{weather_key}"
   @forecast = HTTParty.get(url).parsed_response.to_hash
   @day_number=1
+  
   ### Get the news
   news_key = "506191c9c05f4e31baf54ff587752cee"
   url = "https://newsapi.org/v2/top-headlines?country=us&apiKey=#{news_key}"
